94ac68406d43a81278afcc27be758e38f75dfbc0
Onyx Gautam <onyxgautam01@gmail.com>
2025-08-17 18:36:15 -0400

    Update dir-locals and add pqivrc

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,9 @@
-((nil . ((eval . (add-hook 'after-save-hook
-                           (lambda nil
-                             (when (string-match-p "^~/dotfiles" default-directory)
-                               (shell-command "stow .")))
-                           nil t)))))
+((nil .  ((eval . (add-hook 'after-save-hook
+                            (lambda nil
+                              (let ((base-dir (expand-file-name "~/dotfiles")))
+                                (when (string-match-p
+                                       (concat "^" base-dir)
+                                       default-directory)
+                                  (let ((default-directory base-dir))
+                                    (shell-command "stow .")))))
+                            nil t)))))
